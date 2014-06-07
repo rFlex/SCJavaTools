ea22190fb92b513be7557b26cd5cede5793ed47e
Simon CORSIN <simon@corsin.me>
2014-06-07 11:33:46 -0400

    Added action for using a lambda that returns void easily.

---
+++ src/main/java/me/corsin/javatools/misc/Action.java
@@ -0,0 +1,16 @@
+/////////////////////////////////////////////////
+// Project : SCJavaTools
+// Package : me.corsin.javatools.misc
+// Action.java
+//
+// Author : Simon CORSIN <simoncorsin@gmail.com>
+// File created on Jun 4, 2014 at 3:25:03 PM
+////////
+
+package me.corsin.javatools.misc;
+
+public interface Action<T> {
+
+	void run(T param);
+	
+}
